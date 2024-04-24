3de032bc5d46a810ebdb6a6aa36b2a443a7ef543
Lucy D <126295672+lucinda-d@users.noreply.github.com>
2024-04-23 20:10:51 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Change Instagram icon color - tomato was not in quotation marks
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "tomato";
+
+// Add background image - foodie class was actually .food
+var foodImage = document.querySelector(".food");
+foodImage.style.backgroundImage = "url('../img/victoria-shes-unsplash.jpg')";
+
+// Dotted border-bottom around h1  - variable name for style noms instead of h1
+var noms = document.querySelector("h1");
+noms.style.borderBottom = "4px dotted tomato";
